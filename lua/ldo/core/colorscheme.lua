7f0f7ab0f10238df4b8265e58dc9e418e9867777
Pineapple <pineappleman@skiff.com>
2023-01-04 17:24:35 -0300

    feat💅: changed colorscheme

--- lua/ldo/core/colorscheme.lua
+++ lua/ldo/core/colorscheme.lua
@@ -1,5 +1,7 @@
-local status, _ = pcall(vim.cmd, "colorscheme nightfly")
+local status, _ = pcall(vim.cmd, "colorscheme tokyonight-night")
+
 if not status then
   print("Colorscheme not found!")
   return
 end
+
