56f8878a3adc78113dc08bd0c31fc774a9b1d212
Pineapple <pineappleman@skiff.com>
2023-01-07 11:20:34 -0300

    feat🕰️: install wakatime

--- lua/ldo/plugins-setup.lua
+++ lua/ldo/plugins-setup.lua
@@ -101,6 +101,9 @@ return packer.startup(function(use)
 	-- git integration
 	use("lewis6991/gitsigns.nvim") -- show line modifications on left hand side
 
+  -- Wakatime to get developments metrics
+  use("wakatime/vim-wakatime")
+
 	if packer_bootstrap then
 		require("packer").sync()
 	end
