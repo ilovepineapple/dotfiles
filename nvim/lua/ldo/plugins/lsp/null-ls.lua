aa34ceaf78b8f4e61bbdfd52344957e9df27df71
Pineapple <pineappleman@skiff.com>
2023-03-24 20:11:17 -0300

    fix: update null-ls setup to enable option when save a file

--- nvim/lua/ldo/plugins/lsp/null-ls.lua
+++ nvim/lua/ldo/plugins/lsp/null-ls.lua
@@ -25,7 +25,8 @@ null_ls.setup({
     }), -- lua formatter
     formatting.rubocop,
     diagnostics.rubocop,
-    diagnostics.eslint_d.with({ -- js/ts linter
+    diagnostics.eslint_d.with({
+      -- js/ts linter
       -- only enable eslint if root has .eslintrc.js (not in youtube nvim video)
       condition = function(utils)
         return utils.root_has_file(".eslintrc.js") -- change file extension if you use something else
@@ -33,20 +34,15 @@ null_ls.setup({
     }),
   },
   -- configure format on save
-  on_attach = function(current_client, bufnr)
-    if current_client.supports_method("textDocument/formatting") then
+  on_attach = function(client, bufnr)
+    if client.supports_method("textDocument/formatting") then
       vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
       vim.api.nvim_create_autocmd("BufWritePre", {
         group = augroup,
         buffer = bufnr,
         callback = function()
-          vim.lsp.buf.format({
-            filter = function(client)
-              --  only use null-ls for formatting instead of lsp server
-              return client.name == "null-ls"
-            end,
-            bufnr = bufnr,
-          })
+          -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+          vim.lsp.buf.formatting_sync()
         end,
       })
     end
